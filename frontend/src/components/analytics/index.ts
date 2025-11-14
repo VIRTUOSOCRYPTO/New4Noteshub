4b97b91f94f5cdbf0784de4c2194d9c54bf58ae1
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2025-11-14 17:13:03 +0530

    Create index.ts

---
+++ frontend/src/components/analytics/index.ts
@@ -0,0 +1,4 @@
+export { DashboardStats } from './DashboardStats';
+export { UploadTrendsChart } from './UploadTrendsChart';
+export { PopularNotesTable } from './PopularNotesTable';
+export { DepartmentStatsChart } from './DepartmentStatsChart';
