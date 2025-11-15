7b16262bdb57cb81af257e29d8e2160c3209cbdd
Yashavanth H S <108397456+VIRTUOSOCRYPTO@users.noreply.github.com>
2025-11-15 17:05:17 +0530

    Create index.ts

---
+++ frontend/src/components/viral/index.ts
@@ -0,0 +1,6 @@
+// Export all viral growth components
+export { StreakTracker } from './StreakTracker';
+export { PointsDisplay } from './PointsDisplay';
+export { Leaderboard } from './Leaderboard';
+export { ReferralDashboard } from './ReferralDashboard';
+export { ShareButtons } from './ShareButtons';
